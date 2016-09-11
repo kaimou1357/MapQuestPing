b90af79edf8985179df21cb0d53145f80c7829bb
kaimou <kaimou1357>
2016-09-11 03:09:10 -0700

    Initial commit

---
+++ app/src/main/java/com/example/kaimou/mapquestping/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.kaimou.mapquestping;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.mapbox.mapboxsdk.annotations.MarkerOptions;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
+import com.mapquest.android.commoncore.model.LatLng;
+import com.mapquest.mapping.maps.MapView;
+import com.mapquest.mapping.maps.MapboxMap;
+import com.mapquest.mapping.maps.OnMapReadyCallback;
+
+public class MainActivity extends AppCompatActivity {
+    private MapboxMap mMapboxMap;
+    private MapView mMapView;
+    private final com.mapbox.mapboxsdk.geometry.LatLng SAN_FRAN = new com.mapbox.mapboxsdk.geometry.LatLng(37.7749, -122.4194);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mMapView = (MapView) findViewById(R.id.mapquestMapView);
+        mMapView.onCreate(savedInstanceState);
+
+        mMapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(MapboxMap mapboxMap) {
+                mMapboxMap = mapboxMap;
+                mMapboxMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new com.mapbox.mapboxsdk.geometry.LatLng(37.775889, -122.386657), 11));
+                addMarker(mMapboxMap);
+            }
+
+        });
+    }
+
+    private void addMarker(MapboxMap mapboxMap) {
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(SAN_FRAN);
+        markerOptions.title("San Francisco");
+        markerOptions.snippet("Welcome to San Fran!");
+        mapboxMap.addMarker(markerOptions);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
